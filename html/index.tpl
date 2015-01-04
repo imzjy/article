08216d8cf7731569a302691afff618a5ad7f38ce
Jerry Chou <imjatsz@gmail.com>
2015-01-04 10:13:47 +0800

    add css to blog articles

---
+++ html/index.tpl
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>博客 | 周继元</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <header class="page-header">
+                <h1>周继元的博客</h1>
+        </header>
+
+        <section class="row">
+            <div class="col-xs-12">
+                <ul>
+                    ${aritcle_list}
+                </ul>
+            </div>
+        </section>
+
+        <footer>
+            <div class="row">
+                <div class="col-xs-12 pull-right">
+                    imjatsz@2015    
+                </div>
+            </div>
+        </footer>
+
+    </div>
+</body>
+</html>
--- md2html.py
+++ md2html.py
@@ -5,8 +5,16 @@ import os
 import re
 import codecs
 import markdown
+from string import Template
 
-OUTPUT_DIR = './html'
+# globals
+CURPATH = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_DIR = os.path.join(CURPATH, 'html')
+
+def get_tpl(tpl_name):
+    with codecs.open(os.path.join(OUTPUT_DIR, tpl_name), 'r', 'utf-8') as f:
+        index_tpl = f.read()
+        return (index_tpl or 'no content').strip()
 
 def get_markdown_files():
     results = []
@@ -22,7 +30,15 @@ def get_markdown_files():
 
     return results
 
+def get_article_title(article_md_file):
+    with codecs.open(article_md_file, 'r', 'utf-8') as f:
+        title = f.readline()
+        return (title or 'untitled').strip()
+
 def markdown_to_html(markdown_files):
+
+    entry_tpl = Template(get_tpl('entry.tpl'))
+
     for path, name in markdown_files:
 
         #create folder
@@ -39,42 +55,22 @@ def markdown_to_html(markdown_files):
             codecs.open(dest_full_name, 'w', 'utf-8') as dest:
             
             md_text = src.read()
-            html_text = markdown.markdown(md_text, extensions=['markdown.extensions.fenced_code'])
-            dest.write(html_text)
+            article_html = markdown.markdown(md_text, extensions=['markdown.extensions.fenced_code'])
+            dest.write(entry_tpl.substitute(article=article_html))
             # print html_text
-
 
 def generate_index_html(markdown_files):
 
     markdown_files_in_full_name = [os.path.join(d, f) for d, f in markdown_files]
 
-    start_html = u'''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>blog</title>
-            <meta http-equiv="content-type" content="text/html;charset=utf-8">
-        </head>
-        <body>
-        <div class="container">
-        <ol>
-
-    '''
-
+    article_list = ''
     for full_name in reversed(sorted(markdown_files_in_full_name)):
-        with codecs.open(full_name, 'r', 'utf-8') as f:
-            article_url = full_name.rstrip('.md') + '.html'
-            start_html += u'<li><a href="{0}">{1}</a></li>\n'.format(article_url, f.readline().strip())
+        article_url = full_name.rstrip('.md') + '.html'
+        article_list += u'<li><a href="{0}">{1}</a></li>\n'.format(article_url, get_article_title(full_name))
 
-    end_html = u'''
-        </ol>
-        </div>
-        </body>
-        </html>
-    '''
-
+    index_tpl = Template(get_tpl('index.tpl'))
     with codecs.open(os.path.join(OUTPUT_DIR, 'index.html'), 'w+', 'utf-8') as f:
-        f.write(start_html + end_html)
+        f.write(index_tpl.substitute(aritcle_list=article_list))
 
 
 def main():
